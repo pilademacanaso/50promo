c63333faa5f1df7ef88cc10fa3e6bd411a6c8d88
unknown <rubenariza1965@gmail.com>
2025-05-26 12:36:39 -0500

    Primer commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, redirect, request
+import string
+import random
+
+app = Flask(__name__)  # <- CORRECTO
+
+# Diccionario para almacenar URLs acortadas
+url_mapping = {}
+
+def generate_short_url():
+    characters = string.ascii_letters + string.digits
+    short_url = ''.join(random.choices(characters, k=6))
+    return short_url
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        original_url = request.form['url']
+        short_url = generate_short_url()
+        url_mapping[short_url] = original_url
+        return f'Short URL: <a href="/{short_url}">/{short_url}</a>'
+    return '''
+        <form method="post">
+            URL to shorten: <input type="text" name="url">
+            <input type="submit" value="Shorten">
+        </form>
+    '''
+
+@app.route('/<short_url>')
+def redirect_to_url(short_url):
+    original_url = url_mapping.get(short_url)
+    if original_url:
+        return redirect(original_url)
+    return 'URL not found', 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
